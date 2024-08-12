af76c3843025916cb55659559fc03ec428100f32
Beau Malroney <bamalroney@outlook.com>
2024-08-11 19:22:45 -0500

    init

---
+++ speech.py
@@ -0,0 +1,42 @@
+# Python program to listen for user speech, convert to text and then repeat the text to the user.
+
+import speech_recognition as sr
+import pyttsx3 
+
+# Initialize the recognizer 
+r = sr.Recognizer() 
+
+# Convert text to speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command) 
+    engine.runAndWait()
+    
+    
+# Loop infinitely: User's Speech will be converted to Text
+
+while(1):    
+    
+    try:
+        # Use Microphone as input
+        with sr.Microphone() as source2:
+            
+            # Recognizer will adjust for surrounding noise
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            
+            # Listen for user speech 
+            audio2 = r.listen(source2)
+            
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print("Recorded speech", MyText)
+            SpeakText(MyText)
+            
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        
+    except sr.UnknownValueError:
+        print("unknown error occurred")
